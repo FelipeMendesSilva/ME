812d7e1dc03b4ab2c6f8a26220c8d1e862995d01
FelipeMendesSilva <febasmendes@gmail.com>
2026-01-05 02:32:44 -0300

    teste create handler

--- ME.SuperHero.API/ME.SuperHeroTests/BaseTest.cs
+++ ME.SuperHero.API/ME.SuperHeroTests/BaseTest.cs
@@ -1,7 +1,27 @@
+using ME.SuperHero.Application.Handlers.Command;
+using ME.SuperHero.Domain.Interfaces;
+using Moq;
+using System.Reflection.Metadata;
+
 namespace ME.SuperHeroTests
 {
     public class BaseTest
     {
-        
+        protected readonly Mock<IHeroisRepository> _heroisRepoMock;
+        protected readonly Mock<IHeroisSuperpoderesRepository> _heroisSupRepoMock;
+        protected readonly CreateHeroiCommandHandler _createHandler;
+        protected readonly Mock<IUow> _uowMock;
+
+        public BaseTest()
+        {
+            _heroisRepoMock = new Mock<IHeroisRepository>();
+            _heroisSupRepoMock = new Mock<IHeroisSuperpoderesRepository>();
+            _uowMock = new Mock<IUow>();
+            _createHandler = new CreateHeroiCommandHandler(
+                _heroisRepoMock.Object,
+                _heroisSupRepoMock.Object,
+                _uowMock.Object
+            );
+        }
     }
 }
---
+++ ME.SuperHero.API/ME.SuperHeroTests/CommandHandlers/CreateHeroiCommandHandlerTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using ME.SuperHero.Application.Handlers.Command;
+using ME.SuperHero.Application.Requests;
+using ME.SuperHero.Domain.Entities;
+using ME.SuperHero.Domain.Interfaces;
+using ME.SuperHero.Domain.Result;
+using ME.SuperHeroTests;
+using Moq;
+using Xunit;
+
+namespace ME.SuperHero.Tests.Handlers
+{
+    public class CreateHeroiCommandHandlerTests : BaseTest
+    {
+        [Fact]
+        public async Task Handle_ShouldReturnFailure_WhenHeroiAlreadyExists()
+        {
+            // Arrange
+            var request = new RequestCreateHeroi
+            {
+                Nome = "Clark Kent",
+                NomeHeroi = "Superman",
+                Altura = 1.9,
+                Peso = 95,
+                DataNascimento = null,
+                Superpoderes = new List<int>() { 1, 2, 3 }
+            };
+
+            _heroisRepoMock
+                .Setup(r => r.ExistsHeroiByNameAsync(request.NomeHeroi, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _createHandler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess());
+            Assert.Equal("NomeHeroi already exists", result.ErrorMessage);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnSuccess_WhenHeroiIsCreated()
+        {
+            // Arrange
+            var request = new RequestCreateHeroi
+            {
+                Nome = "Clark Kent",
+                NomeHeroi = "Superman",
+                Altura = 1.9,
+                Peso = 95,
+                DataNascimento = null,
+                Superpoderes = new List<int>() { 1, 2, 3 }
+            };
+
+            _heroisRepoMock
+                .Setup(r => r.ExistsHeroiByNameAsync(request.NomeHeroi, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            _heroisRepoMock
+                .Setup(r => r.CreateAsync(It.IsAny<Herois>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            _uowMock
+                .Setup(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _createHandler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess());
+            Assert.Equal("Created", result.Data);
+            Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+            _heroisSupRepoMock.Verify(r => r.AddPowerAsync(It.IsAny<HeroisSuperpoderes>(), It.IsAny<CancellationToken>()), Times.Exactly(request.Superpoderes.Count));
+            _uowMock.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnFailure_WhenCreateAsyncFails()
+        {
+            // Arrange
+            var request = new RequestCreateHeroi
+            {
+                Nome = "Clark Kent",
+                NomeHeroi = "Superman",
+                Altura = 1.9,
+                Peso = 95,
+                DataNascimento = null,
+                Superpoderes = new List<int>() { 1, 2, 3 }
+            };
+
+            _heroisRepoMock
+                .Setup(r => r.ExistsHeroiByNameAsync(request.NomeHeroi, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            _heroisRepoMock
+                .Setup(r => r.CreateAsync(It.IsAny<Herois>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            // Act
+            var result = await _createHandler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess());
+            Assert.Equal("Creating not completed", result.ErrorMessage);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+    }
+}
